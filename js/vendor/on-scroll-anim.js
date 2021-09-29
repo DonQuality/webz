4f31f4275d95dc556f2bae7f724d53b5b96f8b08
ItzxQuality <itzxquality@ItzxQualitys-MacBook-Pro.local>
2021-09-29 10:21:39 -0400

    first commit

---
+++ js/vendor/on-scroll-anim.js
@@ -0,0 +1,31 @@
+// var tl = new TimelineLite();
+
+function onScrollAnim() {
+
+  var docScroll = $(document).scrollTop(),
+
+  heroSection = $(".el-hero-section").offset().top + 250;
+
+  if (docScroll > heroSection) {
+    $('.el-main-header').css({
+      "background":"#17191b36"
+    });
+  } else{
+    $('.el-main-header').css({
+      "background": ""
+    });
+  }
+
+  if (docScroll > heroSection) {
+    $('.el-main-header').css({
+      "background":"#17191b36"
+    });
+  } else{
+    $('.el-main-header').css({
+      "background": ""
+    });
+  }
+
+}
+
+window.addEventListener("scroll", onScrollAnim);
